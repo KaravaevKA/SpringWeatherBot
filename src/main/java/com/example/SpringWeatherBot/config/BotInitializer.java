fb64daf87f42caae39aba47029a575f718a69a56
Kirill Karavaev <coffee.2016@yandex.ru>
2023-08-12 23:09:43 +0300

    Add main menu and log

--- src/main/java/com/example/SpringWeatherBot/config/BotInitializer.java
+++ src/main/java/com/example/SpringWeatherBot/config/BotInitializer.java
@@ -1,6 +1,7 @@
 package com.example.SpringWeatherBot.config;
 
 import com.example.SpringWeatherBot.service.TelegramBot;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -9,8 +10,10 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
 @Component
 public class BotInitializer {
+
     @Autowired
     TelegramBot bot;
 
@@ -21,7 +24,7 @@ public class BotInitializer {
             telegramBotsApi.registerBot(bot);
         }
         catch (TelegramApiException e){
-
+        log.error("Error occurred: " + e.getMessage());
         }
     }
 }
--- src/main/java/com/example/SpringWeatherBot/service/TelegramBot.java
+++ src/main/java/com/example/SpringWeatherBot/service/TelegramBot.java
@@ -1,19 +1,40 @@
 package com.example.SpringWeatherBot.service;
 
 import com.example.SpringWeatherBot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
 
-    public TelegramBot(BotConfig config){
+    static final String HELP_TEXT = "This is a demo bot. It was created to practise. You can choose commands in the menu button.";
+
+    public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete your data"));
+        listOfCommands.add(new BotCommand("/help", "get some info from bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list" + e.getMessage());
+        }
     }
 
     @Override
@@ -29,26 +50,32 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
-        if(update.hasMessage() && update.getMessage().hasText()){
+        if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-            switch (messageText){
+            switch (messageText) {
                 case "/start":
 
-                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                        break;
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
                 default:
 
-                        sendMessage(chatId,"Sorry, command was not recognized");
+                    sendMessage(chatId, "Sorry, command was not recognized");
 
             }
         }
 
     }
-    private void startCommandReceived(long chatId, String name)  {
-        String answer = "Hi, " + name+ ", nice to meet you!";
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hi, " + name + ", nice to meet you!";
         sendMessage(chatId, answer);
+        log.info("Replied to user: " + name);
     }
+
     private void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
@@ -57,7 +84,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         try {
             execute(message);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            log.error("Error occurred: " + e.getMessage());
         }
 
     }
